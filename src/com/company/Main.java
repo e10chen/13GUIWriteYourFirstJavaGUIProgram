6bb0544c46b1afcd21beaa9d04600e989c1c8219
Ethan <ethanchen229@gmail.com>
2021-02-24 10:06:07 -0500

    Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 import javafx.application.Application;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -41,6 +38,9 @@ public class Main extends Application {
     Image Robux2;
     ImageView Robux2View;
     ToggleButton toggleImage;
+    ToggleButton toggleImage2;
+    ToggleButton toggleImage3;
+    ToggleGroup toggleImageGroup;
     FileInputStream Robux3image;
     Image Robux3;
     ImageView Robux3View;
@@ -73,18 +73,32 @@ public class Main extends Application {
         RobuxImage = new FileInputStream("C:\\Users\\Ethan\\Desktop\\RobuxImage.png");
         RobuxImage1 = new Image(RobuxImage);
         RobuxImageView = new ImageView(RobuxImage1);
+        RobuxImageView.setFitHeight(100);
+        RobuxImageView.setFitWidth(100);
 
         RobloxLogoImage = new FileInputStream("C:\\Users\\Ethan\\Desktop\\Roblox New Logo.png");
         RobloxLogo = new Image(RobloxLogoImage);
         RobloxLogoView = new ImageView(RobloxLogo);
+        RobloxLogoView.setFitHeight(100);
+        RobloxLogoView.setFitWidth(100);
 
         Robux2image = new FileInputStream("C:\\Users\\Ethan\\Desktop\\Robux2.png");
         Robux2 = new Image(Robux2image);
         Robux2View = new ImageView(Robux2);
+        Robux2View.setFitHeight(100);
+        Robux2View.setFitWidth(100);
 
         Robux3image = new FileInputStream("C:\\Users\\Ethan\\Desktop\\Robux3.jpg");
+        Robux3 = new Image(Robux3image);
+        Robux3View = new ImageView(Robux3);
+        Robux3View.setFitHeight(100);
+        Robux3View.setFitWidth(100);
 
         Robux4image =  new FileInputStream("C:\\Users\\Ethan\\Desktop\\Robux4.jpg");
+        Robux4 = new Image(Robux4image);
+        Robux4View = new ImageView(Robux4);
+        Robux4View.setFitHeight(100);
+        Robux4View.setFitWidth(100);
 
 
             // Checkboxes
@@ -103,10 +117,28 @@ public class Main extends Application {
         });
 
         //Toggle imagae
-        toggleImage = new ToggleButton("Swtich Image");
+        toggleImage = new ToggleButton("Image1");
+        toggleImage2 = new ToggleButton("Image2");
+        toggleImage3 = new ToggleButton("Image3");
+        toggleImageGroup = new ToggleGroup();
+        toggleImage.setToggleGroup(toggleImageGroup);
+        toggleImage2.setToggleGroup(toggleImageGroup);
+        toggleImage3.setToggleGroup(toggleImageGroup);
+
+
         toggleImage.setOnAction(actionEvent -> {
             boolean isSelected = toggleImage.isSelected();
             Robux2View.setVisible(isSelected);
+        });
+
+        toggleImage2.setOnAction(actionEvent -> {
+            boolean isSelected = toggleImage2.isSelected();
+            Robux3View.setVisible(isSelected);
+        });
+
+        toggleImage3.setOnAction(actionEvent -> {
+            boolean isSelected = toggleImage3.isSelected();
+            Robux4View.setVisible(isSelected);
         });
 
             // Text
@@ -115,14 +147,14 @@ public class Main extends Application {
 
 
             //create Layout
-        VBox myLayout = new VBox( RobloxLogoView, RobuxImageView, Robux2View, toggleImage, robuxText, option1, option2, option3, Text, myFirstButton, myFirstCheckBox);
+        VBox myLayout = new VBox( RobloxLogoView, RobuxImageView, Robux2View, toggleImage, Robux3View, toggleImage2, Robux4View, toggleImage3, robuxText, option1, option2, option3, Text, myFirstButton, myFirstCheckBox);
 
         // create Scene
         Scene myFirstScene = new Scene(myLayout);
         stage.setScene(myFirstScene);
 
-        stage.setWidth(600);
-        stage.setHeight(300);
+        stage.setWidth(300);
+        stage.setHeight(500);
 
         myFirstScene.setCursor(Cursor.CROSSHAIR);
 
